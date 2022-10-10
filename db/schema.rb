54b0a0509bbf309e75d5f7e17c625004d47ec46f
abhishek <abhishek.mishra@w3villa.com>
2022-10-10 11:44:14 +0530

    changed db

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_10_07_053349) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "streams", force: :cascade do |t|
     t.integer "user_id", null: false
     t.text "desc"
